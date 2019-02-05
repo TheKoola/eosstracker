ff13193b631e63c8f1e07855a9fbab42ad4b863d
deatojef <deatojef@yahoo.com>
2019-02-05 13:53:07 -0700

    Updating license info

--- www/map.php
+++ www/map.php
@@ -1,4 +1,28 @@
 <?php
+/*
+*
+##################################################
+#    This file is part of the HABTracker project for tracking high altitude balloons.
+#
+#    Copyright (C) 2019, Jeff Deaton (N6BA)
+#
+#    HABTracker is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    HABTracker is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with HABTracker.  If not, see <https://www.gnu.org/licenses/>.
+#
+##################################################
+*
+ */
+
 
     session_start();
     $pagetitle="APRS:  Map";
@@ -1231,7 +1255,7 @@ function getTrackers() {
                 <p class="logo" style="margin-top:  30px; margin-bottom: 0px;"><?php if (isset($logo)) printf("%s", $logo); else printf("No Logo"); ?><br>
                 </p>
                 <p style="margin-top: 0px; font-size: 1.1em; color:  #0052cc; font-style:  italic; text-shadow: 5px 5px 10px gray; margin-bottom:  30px;">Tracking High Altitude Balloons</p>
-                <p class="lorem">Welcome to the Map utilty for the high altitude balloon tracker application.  From these screens one can monitor positions of APRS objects and their repsective paths.</p>
+                <p class="lorem">Welcome to the map utilty for the HAB Tracker application.  From these screens one can monitor positions of APRS objects and their repsective paths.</p>
                 <p class="section-header">System: &nbsp;  <?php echo $_SERVER["HTTP_HOST"]; ?>
                     <br>System Status: <span id="systemstatus"></span></p>
                 <table class="packetlist">
