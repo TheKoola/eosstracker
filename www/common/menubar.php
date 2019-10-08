cda2d48beeeaa0d58f05b783c4eafa8cb8d78ceb
Jeff Deaton <deatojef@yahoo.com>
2019-10-08 09:59:32 -0600

    Option added to the menu for a link to the ADS-B, locally hosted dump1090-fa web page

--- www/common/menubar.php
+++ www/common/menubar.php
@@ -39,6 +39,34 @@
                     <td align="center"><a href="/setup.php" class="navbar">Setup</a></td>
                     <td align="center"><a href="/rawdata.php" class="navbar">Data</a></td>
                     <td align="center"><a href="/dashboard.php" target="_blank" class="navbar">Dashboard</a></td>
+<?php
+                    # Specify that we don't care about self-signed SSL certs
+                    $context = stream_context_create( [
+                        'ssl' => [
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        ],
+                    ]);
+
+                    # If the dump1090-fa package has been installed, then this should be its URL
+                    $dump1090fa_url = "https://" . $_SERVER["HTTP_HOST"] . "/dump1090-fa";
+
+                    # Get HTML headers by trying to load the URL 
+                    $file_headers = get_headers($dump1090fa_url, 0, $context);
+
+                    # Check if the dump1090-fa package has been installed
+                    if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') 
+                        $exists = false;
+                    else 
+                        $exists = true;
+
+                    # If the URL exists, then we add a link to the FlightAware page on the menubar
+                    if ($exists) {
+?>
+                    <td align="center"><a href="/dump1090-fa" target="_blank" class="navbar">ADS-B</a></td>
+<?php
+                    }
+?>
                     <td align="center"><a href="/map.php" target="_blank" class="navbar">Map</a></td>
                     <td align="center"><a href="/about.php" class="navbar">About</a></td>
                 </tr>
