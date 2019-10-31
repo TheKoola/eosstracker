c57072585880ea9ba240987f21bf3a117c38e4b2
Jeff Deaton <deatojef@yahoo.com>
2019-10-31 16:07:42 -0600

    TEMP: updates to startadsb.bash

--- sbin/startadsb.bash
+++ sbin/startadsb.bash
@@ -3,5 +3,5 @@
 # EOSS ICAO number for reference
 icao="A59EE9"
 
-nohup /eosstracker/sbin/adsbclient.py ${icao} > /eosstracker/logs/adsbclient_${icao}.out &
-nohup /eosstracker/sbin/eoss_adsb.bash  &
+nohup /eosstracker/sbin/adsbclient.py ${icao} > /eosstracker/logs/adsbclient_${icao}.out 2>&1 &
+nohup /eosstracker/sbin/eoss_adsb.bash >/dev/null 2>&1 &
