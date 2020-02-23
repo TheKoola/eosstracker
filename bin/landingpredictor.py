bba35ff322f41e66ee1b11d606814613ca70cb4f
Jeff Deaton <deatojef@yahoo.com>
2020-02-22 19:05:12 -0700

    Incorrect syntax calling math methods within the distance function

--- bin/landingpredictor.py
+++ bin/landingpredictor.py
@@ -147,20 +147,16 @@ class LandingPredictor:
     # Function to use to determine distance between two points
     #####################################
     def distance(self, lat1, lon1, lat2, lon2):
-        #p = 0.017453292519943295     #Pi/180
-        #a = 0.5 - math.cos((lat2 - lat1) * p)/2 + math.cos(lat1 * p) * math.cos(lat2 * p) * (1 - math.cos((lon2 - lon1) * p)) / 2
-        #return * math.asin(math.sqrt(a)) #2*R*asin...
-
-        lon1 = radians(lon1)
-        lon2 = radians(lon2)
-        lat1 = radians(lat1)
-        lat2 = radians(lat2)
+        lon1 = math.radians(lon1)
+        lon2 = math.radians(lon2)
+        lat1 = math.radians(lat1)
+        lat2 = math.radians(lat2)
 
         # Haversine formula
         dlon = lon2 - lon1
         dlat = lat2 - lat1
-        a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-        c = 2 * asin(sqrt(a))
+        a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+        c = 2 * math.asin(math.sqrt(a))
         #r = 6371 # Radius of earth in kilometers. Use 3956 for miles
         r = 3956 # Radius of earth in kilometers. Use 3956 for miles
 
