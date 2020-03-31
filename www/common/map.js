3ff69a3cdff6cfcd65427dae1081c23f42afb1db
Jeff Deaton <deatojef@yahoo.com>
2020-03-31 11:05:37 -0600

    Change Internet discovered stations and weather stations layers to use clustering to reduce map clutter and to better match the kiosk version.

--- www/common/map.js
+++ www/common/map.js
@@ -1569,11 +1569,12 @@ function getTrackers() {
     ***********/
     function initialize_layers() {
         // Layer groups for all stations and just my station.  This allows toggling the visibility of these two groups of objects.
-        //var allstations = L.markerClusterGroup();
-        var allstations = L.layerGroup();
+        var allstations = L.markerClusterGroup();
+        //var allstations = L.layerGroup();
         var allrfstations = L.layerGroup();
         var mystation = L.layerGroup();
-        var wxstations = L.layerGroup();
+        //var wxstations = L.layerGroup();
+        var wxstations = L.markerClusterGroup();
 
         // Layer group for trackers that are not assigned to a specific flight
         var trackersatlarge = L.layerGroup();
