ca14c81aedec80210b3946416a183c7c38219af7
Jeff Deaton <deatojef@yahoo.com>
2020-08-21 08:16:55 -0600

    Updates to the SQL for getting a list of the latest packets for a beacon.

--- bin/landingpredictor.py
+++ bin/landingpredictor.py
@@ -941,24 +941,25 @@ class LandingPredictor(PredictorBase):
         # The SQL statement to get the latest packets for this callsign
         # columns:  timestamp, altitude, latitude, longitude
         # Note:  only those packetst that might have occured within the last 6hrs are queried.
-        latestpackets_sql = """select
-                y.thetime,
+        latestpackets_sql = """
+            select
+                y.packet_time as thetime,
                 round(y.altitude) as altitude,
                 round(y.lat, 6) as latitude,
                 round(y.lon, 6) as longitude,
                 case when y.delta_secs > 0 then
                     (y.altitude - y.previous_altitude) / y.delta_secs
-                else 
+                else
                     0
                 end as vert_rate,
                 case when y.delta_secs > 0 then
                     (y.lat - y.previous_lat) / y.delta_secs
-                else 
+                else
                     0
                 end as lat_rate,
                 case when y.delta_secs > 0 then
                     (y.lon - y.previous_lon) / y.delta_secs
-                else 
+                else
                     0
                 end as lon_rate,
                 round(y.elapsed_secs / 60.0) as elapsed_mins,
@@ -972,96 +973,272 @@ class LandingPredictor(PredictorBase):
                     NULL
                 end as air_density_slugs_per_ft3
 
-
-            from
-                (
-                select
-                    z.thetime,
-                    z.callsign,
-                    z.altitude,
-                    z.comment,
-                    z.symbol,
-                    z.speed_mph,
-                    z.bearing,
-                    z.lat,
-                    z.lon,
-                    z.temperature_k,
-                    z.pressure_pa,
-                    z.ptype, 
-                    z.hash,
-                    z.raw,
-                    lag(z.altitude, 1) over(order by z.thetime)  as previous_altitude,
-                    lag(z.lat, 1) over (order by z.thetime) as previous_lat,
-                    lag(z.lon, 1) over (order by z.thetime) as previous_lon,
-                    extract ('epoch' from (z.thetime - lag(z.thetime, 1) over (order by z.thetime))) as delta_secs,
-                    extract ('epoch' from (now() - z.tm)) as elapsed_secs
-
                 from 
-                    (
+                (
                     select
-                        a.tm,
-                        case
-                            when a.raw similar to '%%[0-9]{6}h%%' then
-                                date_trunc('milliseconds', ((to_timestamp(now()::date || ' ' || substring(a.raw from position('h' in a.raw) - 6 for 6), 'YYYY-MM-DD HH24MISS')::timestamp at time zone 'UTC') at time zone %s)::time)::time without time zone
-                            else
-                                date_trunc('milliseconds', a.tm)::time without time zone
-                        end as thetime,
-                        a.callsign,
-                        a.altitude,
-                        a.comment,
-                        a.symbol,
-                        a.speed_mph,
-                        a.bearing,
-                        cast(st_y(a.location2d) as numeric) as lat,
-                        cast(st_x(a.location2d) as numeric) as lon,
-                        case
-                        when a.raw similar to '%% [-]{0,1}[0-9]{1,6}T[-]{0,1}[0-9]{1,6}P%%' then
-                            round(273.15 + cast(substring(substring(substring(a.raw from ' [-]{0,1}[0-9]{1,6}T[-]{0,1}[0-9]{1,6}P') from ' [-]{0,1}[0-9]{1,6}T') from ' [-]{0,1}[0-9]{1,6}') as decimal) / 10.0, 2)
-                        else
-                            NULL
-                        end as temperature_k,
-                        case
-                            when a.raw similar to '%% [-]{0,1}[0-9]{1,6}T[-]{0,1}[0-9]{1,6}P%%' then
-                                round(abs(cast(substring(substring(a.raw from '[-]{0,1}[0-9]{1,6}P') from '[0-9]{1,6}') as numeric) * 10.0), 2)
+                        c.thetime,
+                        c.packet_time,
+                        c.callsign,
+                        c.flightid,
+                        c.altitude,
+                        c.comment,
+                        c.symbol,
+                        c.speed_mph,
+                        c.bearing,
+                        c.lat,
+                        c.lon,
+                        c.temperature_k,
+                        c.pressure_pa,
+                        c.ptype, 
+                        c.hash,
+                        c.raw,
+                        lag(c.altitude, 1) over(order by c.packet_time)  as previous_altitude,
+                        lag(c.lat, 1) over (order by c.packet_time) as previous_lat,
+                        lag(c.lon, 1) over (order by c.packet_time) as previous_lon,
+                        extract ('epoch' from (c.packet_time - lag(c.packet_time, 1) over (order by c.packet_time))) as delta_secs,
+                        extract ('epoch' from (now()::time - c.thetime)) as elapsed_secs,
+                        c.sourcename,
+                        c.heardfrom,
+                        c.freq,
+                        c.channel,
+                        c.source
+
+                        -- This is the union below here...
+                        from 
+                        (
+                            -- This is the internet-only side of the union
+                            select distinct
+                            date_trunc('milliseconds', a.tm)::time without time zone as thetime,
+                            case
+                                when a.raw similar to '%%[0-9]{6}h%%' then
+                                    date_trunc('milliseconds', ((to_timestamp(now()::date || ' ' || substring(a.raw from position('h' in a.raw) - 6 for 6), 'YYYY-MM-DD HH24MISS')::timestamp at time zone 'UTC') at time zone %s)::time)::time without time zone
+                                else
+                                    date_trunc('milliseconds', a.tm)::time without time zone
+                            end as packet_time,
+                            a.callsign, 
+                            f.flightid,
+                            a.altitude,
+                            a.comment, 
+                            a.symbol, 
+                            a.speed_mph,
+                            a.bearing,
+                            cast(ST_Y(a.location2d) as numeric) as lat,
+                            cast(ST_X(a.location2d) as numeric) as lon,
+                            NULL as sourcename,
+                            NULL as heardfrom,
+                            -1 as freq,
+                            -1 as channel,
+
+                            -- The temperature (if available) from any KC0D packets
+                            case when a.raw similar to '%% [-]{0,1}[0-9]{1,6}T[-]{0,1}[0-9]{1,6}P%%' then
+                                round(273.15 + cast(substring(substring(substring(a.raw from ' [-]{0,1}[0-9]{1,6}T[-]{0,1}[0-9]{1,6}P') from ' [-]{0,1}[0-9]{1,6}T') from ' [-]{0,1}[0-9]{1,6}') as decimal) / 10.0, 2)
                             else
                                 NULL
-                        end as pressure_pa,
-                        a.ptype, 
-                        a.hash,
-                        a.raw
+                            end as temperature_k,
 
-                    from
-                        packets a,
-                        flights f,
-                        flightmap fm
+                            -- The pressure (if available) from any KC0D packets
+                            case
+                                when a.raw similar to '%% [-]{0,1}[0-9]{1,6}T[-]{0,1}[0-9]{1,6}P%%' then
+                                    round(cast(substring(substring(a.raw from '[0-9]{1,6}P') from '[0-9]{1,6}') as decimal) * 10.0, 2)
+                                else
+                                    NULL
+                            end as pressure_pa,
+                            a.ptype,
+                            a.hash,
+                            a.raw,
+                            a.source
 
-                    where 
-                        a.callsign = fm.callsign
-                        and fm.flightid = f.flightid
-                        and f.active = 'y'
-                        and a.tm > (now() - interval '06:00:00')
-                        and a.location2d != ''
-                        and a.altitude > 0
-                        and a.callsign = %s
-                        and a.source = 'other'
+                            from packets a 
+                            left outer join (
+                                select distinct on (z.hash)
+                                z.hash,
+                                z.callsign,
+                                max(z.tm) as thetime
 
-                    order by
-                        thetime,
-                        a.callsign
+                                from
+                                packets z
 
-                    ) as z
+                                where
+                                z.location2d != '' 
+                                and z.tm > (now() - interval '06:00:00')
+                                and z.source = 'direwolf'
 
+                                group by
+                                z.hash,
+                                z.callsign
+
+                                order by
+                                z.hash
+                            ) as dw on dw.callsign = a.callsign and dw.hash = a.hash and dw.thetime >= (a.tm - interval '00:00:08') and dw.thetime < (a.tm + interval '00:00:08'),
+                            flights f,
+                            flightmap fm
+
+                            where 
+                            a.location2d != '' 
+                            and dw.hash is null
+                            and a.tm > (now() - interval '06:00:00')
+                            and a.source = 'other'
+                            and fm.flightid = f.flightid
+                            and f.active = 'y'
+                            and a.callsign = fm.callsign
+
+                        union
+                        
+
+                        -- This is the RF-only side of the union
+                        select 
+                            z.thetime,
+                            z.packet_time,
+                            z.callsign,
+                            z.flightid,
+                            z.altitude,
+                            z.comment,
+                            z.symbol,
+                            z.speed_mph,
+                            z.bearing,
+                            z.lat,
+                            z.lon,
+                            z.sourcename,
+                            case when array_length(z.path, 1) > 0 then
+                                z.path[array_length(z.path, 1)]
+                            else
+                                z.sourcename
+                            end as heardfrom,
+                            z.freq,
+                            z.channel,
+                            z.temperature_k,
+                            z.pressure_pa,
+                            z.ptype,
+                            z.hash,
+                            z.raw,
+                            z.source
+                            
+                            from
+                                (
+                                select distinct
+                                date_trunc('milliseconds', a.tm)::time without time zone as thetime,
+                                case
+                                    when a.raw similar to '%%[0-9]{6}h%%' then
+                                        date_trunc('milliseconds', ((to_timestamp(now()::date || ' ' || substring(a.raw from position('h' in a.raw) - 6 for 6), 'YYYY-MM-DD HH24MISS')::timestamp at time zone 'UTC') at time zone %s)::time)::time without time zone
+                                    else
+                                        date_trunc('milliseconds', a.tm)::time without time zone
+                                end as packet_time,
+                                a.callsign, 
+                                f.flightid,
+                                a.altitude,
+                                a.comment, 
+                                a.symbol, 
+                                a.speed_mph,
+                                a.bearing,
+                                cast(ST_Y(a.location2d) as numeric) as lat,
+                                cast(ST_X(a.location2d) as numeric) as lon,
+
+                                -- This is the source name.  Basically the name of the RF station that we heard this packet from
+                                case when a.raw similar to '[0-9A-Za-z]*[\-]*[0-9]*>%%' then
+                                    split_part(a.raw, '>', 1)
+                                else
+                                    NULL
+                                end as sourcename,
+
+                                -- The frequency this packet was heard on
+                                round(a.frequency / 1000000.0,3) as freq, 
+
+                                -- The Dire Wolf channel
+                                a.channel,
+
+                                -- The ranking of whether this was heard directly or via a digipeater
+                                dense_rank () over (partition by a.hash order by cast(
+                                    cardinality((string_to_array(regexp_replace(split_part(split_part(a.raw, ':', 1), '>', 2), ',WIDE[0-9]*[\-]*[0-9]*', '', 'g'), ','))[2:]) as int) asc, 
+                                    a.channel asc,
+                                    date_trunc('millisecond', a.tm) asc
+                                ), 
+
+                                -- The temperature (if available) from any KC0D packets
+                                case when a.raw similar to '%% [-]{0,1}[0-9]{1,6}T[-]{0,1}[0-9]{1,6}P%%' then
+                                    round(273.15 + cast(substring(substring(substring(a.raw from ' [-]{0,1}[0-9]{1,6}T[-]{0,1}[0-9]{1,6}P') from ' [-]{0,1}[0-9]{1,6}T') from ' [-]{0,1}[0-9]{1,6}') as decimal) / 10.0, 2)
+                                else
+                                    NULL
+                                end as temperature_k,
+
+                                -- The pressure (if available) from any KC0D packets
+                                case
+                                    when a.raw similar to '%% [-]{0,1}[0-9]{1,6}T[-]{0,1}[0-9]{1,6}P%%' then
+                                        round(cast(substring(substring(a.raw from '[0-9]{1,6}P') from '[0-9]{1,6}') as decimal) * 10.0, 2)
+                                    else
+                                        NULL
+                                end as pressure_pa,
+
+                                a.ptype,
+                                a.hash,
+                                a.raw,
+                                case when a.raw similar to '%%>%%:%%' then
+                                    (string_to_array(regexp_replace(split_part(split_part(a.raw, ':', 1), '>', 2), ',WIDE[0-9]*[\-]*[0-9]*', '', 'g'), ','))[2:]
+                                else
+                                    NULL
+                                end as path,
+                                a.source
+
+                                from packets a 
+                                left outer join (
+                                    select distinct on (z.hash)
+                                    z.hash,
+                                    z.callsign,
+                                    max(z.tm) as thetime
+
+                                    from
+                                    packets z
+
+                                    where
+                                    z.location2d != '' 
+                                    and z.tm > (now() - interval '06:00:00')
+                                    and z.source = 'other'
+
+                                    group by
+                                    z.hash,
+                                    z.callsign
+
+                                    order by
+                                    z.hash
+                                ) as dw on dw.callsign = a.callsign and dw.hash = a.hash and dw.thetime >= (a.tm + interval '00:00:08') and dw.thetime < (a.tm + interval '00:00:16'),
+                                flights f,
+                                flightmap fm
+
+                                where 
+                                dw.hash is null
+                                and a.location2d != '' 
+                                and a.tm > (now() - interval '06:00:00')
+                                and fm.flightid = f.flightid
+                                and f.active = 'y'
+                                and a.callsign = fm.callsign
+                                and a.source = 'direwolf'
+
+                                order by 
+                                a.hash,
+                                thetime,
+                                a.callsign) as z
+
+                            where 
+                            z.dense_rank = 1
+
+                            order by
+                            thetime,
+                            callsign
+
+                        ) as c
+                        -- c is the union
+
+                    ) as y
+                    -- y uses the window functions (i.e. lag) to calculate vert, lat, and lon rates.
+
+                where 
+                    y.callsign = %s 
 
                 order by
-                    z.thetime asc,
-                    z.callsign
-                ) as y
-
-
-            order by
-                y.thetime asc
-            ; """
-                      
+                    y.callsign,
+                    y.packet_time asc
+            ;
+        """
+                          
         try:
             # Check if we're connected to the database or not.
             if not self.connectToDatabase():
@@ -1069,7 +1246,7 @@ class LandingPredictor(PredictorBase):
 
             # Execute the SQL statment and get all rows returned
             landingcur = self.landingconn.cursor()
-            landingcur.execute(latestpackets_sql, [ self.timezone, callsign.upper() ])
+            landingcur.execute(latestpackets_sql, [ self.timezone, self.timezone, callsign.upper() ])
             rows = landingcur.fetchall()
             landingcur.close()
 
