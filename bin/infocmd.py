27a3ddaecfa3154cfecfcf2649005734f4e9f76c
Jeff Deaton <deatojef@yahoo.com>
2021-10-15 19:20:48 -0600

    Update copyright date.

--- bin/infocmd.py
+++ bin/infocmd.py
@@ -1,7 +1,7 @@
 ##################################################
 #    This file is part of the HABTracker project for tracking high altitude balloons.
 #
-#    Copyright (C) 2020, Jeff Deaton (N6BA)
+#    Copyright (C) 2020,2021 Jeff Deaton (N6BA)
 #
 #    HABTracker is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
