77c40dc2a32729db1cffeacab8f700735ddc1a17
Jeff Deaton <deatojef@yahoo.com>
2021-11-01 20:20:13 -0600

    Corrections to the TTL display within the sidebar on the map

--- www/getflightdata.php
+++ www/getflightdata.php
@@ -597,7 +597,8 @@
                             landingpredictions l
 
                             where
-                            l.tm > now() - interval '00:10:00'
+                            --l.tm > now() - interval '00:10:00'
+                            l.tm > (now() - (to_char(($5)::interval, 'HH24:MI:SS'))::time)  
                             and l.ttl is not null
 
                             order by
@@ -627,7 +628,7 @@
                         gpsposition g 
 
                         where
-                        g.tm > (now() - (to_char(($5)::interval, 'HH24:MI:SS'))::time)
+                        g.tm > (now() - (to_char(($6)::interval, 'HH24:MI:SS'))::time)
 
                         order by 
                         g.tm desc
@@ -667,6 +668,7 @@
                 sql_escape_string($config["lookbackperiod"] . " minute"), 
                 $get_starttime,
                 sql_escape_string($cs),
+                sql_escape_string($config["lookbackperiod"] . " minute"), 
                 sql_escape_string($config["lookbackperiod"] . " minute"), 
                 )
             );
