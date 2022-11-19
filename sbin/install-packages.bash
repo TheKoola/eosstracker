d334c09f0be3d5ce3f247f98a31e120202a080ad
Jeff Deaton <deatojef@yahoo.com>
2022-11-19 16:29:04 -0700

    initial work on v2 of the brick platform

--- sbin/install-packages.bash
+++ sbin/install-packages.bash
@@ -25,54 +25,47 @@ apt-get -y upgrade
 echo ""
 
 sleep 1
-echo "Installing git and htop..."
-apt-get -y install git git-core htop
-echo ""
-
-sleep 1
 echo "Installing Apache web server..."
-apt-get -y install apache2 apache2-dev
+apt-get -y install apache2 apache2-dev php php-pgsql
 echo ""
 
-sleep 1 
-echo "Installing OpenStreetMap..."
-apt-get -y install libboost-all-dev git-core tar unzip wget bzip2 build-essential autoconf libtool libxml2-dev libgeos-dev libgeos++-dev libpq-dev libbz2-dev libproj-dev munin-node munin libprotobuf-c0-dev protobuf-c-compiler libfreetype6-dev libtiff5-dev libicu-dev libgdal-dev libcairo-dev libcairomm-1.0-dev apache2 apache2-dev libagg-dev liblua5.2-dev ttf-unifont lua5.1 liblua5.1-dev libgeotiff-epsg curl
-echo ""
+#sleep 1 
+#echo "Installing OpenStreetMap..."
+#apt-get -y install libboost-all-dev git-core tar unzip wget bzip2 build-essential autoconf libtool libxml2-dev libgeos-dev libgeos++-dev libpq-dev libbz2-dev libproj-dev munin-node munin libprotobuf-c0-dev protobuf-c-compiler libfreetype6-dev libtiff5-dev libicu-dev libgdal-dev libcairo-dev libcairomm-1.0-dev apache2 apache2-dev libagg-dev liblua5.2-dev ttf-unifont lua5.1 liblua5.1-dev libgeotiff-epsg curl
+#echo ""
 
 sleep 1
 echo "Installing PostgreSQL..."
-apt-get -y install postgresql postgresql-contrib postgis postgresql-10-postgis-2.4 postgresql-10-postgis-scripts
+#apt-get -y install postgresql postgresql-contrib postgis postgresql-10-postgis-2.4 postgresql-10-postgis-scripts
+apt-get -y install postgresql postgis 
 echo ""
 
 sleep 1
 echo "Installing Python libraries..."
-apt-get -y install python-mapnik python-matplotlib python-numpy python-pip python-psutil python-psycopg2 python-scipy python-usb
+#apt-get -y install python-mapnik python-matplotlib python-numpy python-pip python-psutil python-psycopg2 python-scipy python-usb
+apt-get -y install python3-mapnik python3-matplotlib python3-numpy python3-pip python3-psutil python3-psycopg2 python3-scipy python3-usb
 echo ""
 
 sleep 1
 echo "Installing build libraries..."
-apt-get -y install make cmake g++ libboost-dev libboost-system-dev libboost-filesystem-dev libexpat1-dev zlib1g-dev libbz2-dev libpq-dev libgeos-dev libgeos++-dev libproj-dev lua5.2 liblua5.2-dev
+#apt-get -y install make cmake g++ libboost-dev libboost-system-dev libboost-filesystem-dev libexpat1-dev zlib1g-dev libbz2-dev libpq-dev libgeos-dev libgeos++-dev libproj-dev lua5.2 liblua5.2-dev
+apt-get -y install git gcc g++ make cmake libasound2-dev libudev-dev 
 echo ""
 
-sleep 1
-echo "Installing OSM Renderer libraries, tools and fonts..."
-apt-get -y install autoconf apache2-dev libtool libxml2-dev libbz2-dev libgeos-dev libgeos++-dev libproj-dev gdal-bin libmapnik-dev mapnik-utils python-mapnik osmctools
-apt-get -y install fonts-noto-cjk fonts-noto-hinted fonts-noto-unhinted ttf-unifont
-echo ""
-
-sleep 1
-echo "Installing PHP and libraries..."
-apt-get -y install php php-pgsql libwww-perl libjson-xs-perl
-echo ""
+#sleep 1
+#echo "Installing OSM Renderer libraries, tools and fonts..."
+#apt-get -y install autoconf apache2-dev libtool libxml2-dev libbz2-dev libgeos-dev libgeos++-dev libproj-dev gdal-bin libmapnik-dev mapnik-utils python-mapnik osmctools
+#apt-get -y install fonts-noto-cjk fonts-noto-hinted fonts-noto-unhinted ttf-unifont
+#echo ""
 
 sleep 1
 echo "Installing Gnuradio and libraries..."
-apt-get -y install gnuradio gnuradio-dev
+apt-get -y install gnuradio gnuradio-dev gr-osmosdr rtl-sdr airspy 
 echo ""
 
 sleep 1
-echo "Installing GPSd and gr-osmosdr..."
-apt-get -y install gpsd gpsd-clients gr-osmosdr
+echo "Installing GPSD"
+apt-get -y install gpsd gpsd-clients
 echo ""
 
 sleep 1
@@ -81,14 +74,14 @@ apt-get -y install alsa-utils
 apt-get -y remove pulseaudio
 echo ""
 
-sleep 1
-echo "Installing NPM, Node.js and Carto..."
-apt-get -y install npm nodejs
-npm install -g carto
+#sleep 1
+#echo "Installing NPM, Node.js and Carto..."
+#apt-get -y install npm nodejs
+#npm install -g carto
 
 sleep 1
 echo "Installing aprslib..."
-su - -c "pip install aprslib"
+su - -c "pip3 install aprslib"
 echo ""
 
 echo "All packages installed successfully."
