41b2a55d78964247b36d96714b89964d6ac5776a
Jeff Deaton <deatojef@yahoo.cocm>
2023-02-10 16:18:37 -0700

    Updated query for getting a list of packets heard over the prior 5 mins.

--- sql/shortlist.sql
+++ sql/shortlist.sql
@@ -1,7 +1,7 @@
 --##################################################
 --#    This file is part of the HABTracker project for tracking high altitude balloons.
 --#
---#    Copyright (C) 2019, Jeff Deaton (N6BA)
+--#    Copyright (C) 2019,2023 Jeff Deaton (N6BA)
 --#
 --#    HABTracker is free software: you can redistribute it and/or modify
 --#    it under the terms of the GNU General Public License as published by
@@ -18,29 +18,63 @@
 --#
 --##################################################
 
--- Short query to list those APRS packets that have been added to the database over the last 5 mins.
+-- Query to list those APRS packets that have been added to the database over the last 5 mins.
 
-select  distinct 
-date_trunc('second', a.tm)::time without time zone as thetime,
---a.instance || ',' || a.channel as channel,
-a.callsign,
---a.heardfrom,
---a.manufacturer,
-a.comment,
-round(cast(ST_Y(a.location2d) as numeric), 3) || ', ' || round(cast(ST_X(a.location2d) as numeric), 3) as coords,
-round(a.altitude,0) as "alt"
---a.hash
---a.raw
---round(cast(ST_DistanceSphere(ST_GeomFromText('POINT(-104.797435 39.348673)',4326), a.location2d)*.621371/1000 as numeric), 0) as "distance (mi)"
+\pset pager
 
+select 
+date_trunc('second', f.tm)::timestamp without time zone as thetime,
+f.callsign,
+case when array_length(f.path, 1) > 0 then
+    f.path[array_length(f.path, 1)]
+else
+    f.sourcename
+end as heardfrom,
+f.comment,
+f.speed_mph,
+f.bearing,
+f.alt_ft,
+f.coords,
+f.freq_mhz,
+f.source
 
 from
-packets a
+    (select distinct
+    a.tm,
+    a.callsign, 
+    case when a.raw similar to '[0-9A-Za-z]*[\-]*[0-9]*>%' then
+        split_part(a.raw, '>', 1)
+    else
+        NULL
+    end as sourcename,
+    a.comment, 
+    a.ptype,
+    a.bearing,
+    a.source,
+    round(a.speed_mph) as speed_mph,
+    round(a.altitude) as altitude, 
+    round(cast(ST_Y(a.location2d) as numeric), 3) || ', ' || round(cast(ST_X(a.location2d) as numeric), 3) as coords,
+    round(a.altitude,0) as "alt_ft",
+    a.symbol,
+    round(a.frequency / 1000000.0,3) as freq_mhz, 
+    case when a.raw similar to '%>%:%' then
+        (string_to_array(regexp_replace(split_part(split_part(a.raw, ':', 1), '>', 2), ',WIDE[0-9]*[\-]*[0-9]*', '', 'g'), ','))[2:]
+    else
+        NULL
+    end as path
 
-where 
-a.tm > (now() - time '00:05:00')
+    from 
+    packets a
 
-order by 
-1 asc,
-2
+    where 
+    a.tm > (now() - interval '00:05:00')
+
+    order by 
+    a.tm,
+    a.callsign) as f
+
+order by
+thetime asc,
+f.callsign,
+f.source
 ;
