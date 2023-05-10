1ca11f6438c68f0bee3aa17e160435c5a62eaa4e
Jeff Deaton <deatojef@yahoo.com>
2023-05-10 16:39:33 -0600

    udpates to render job

--- sbin/render-huntsville.bash
+++ sbin/render-huntsville.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-render_list -a -m maps -n 4 -z 4 -Z 4 -x 4 -X 4 -y 6 -Y 6
-render_list -a -m maps -n 4 -z 5 -Z 5 -x 8 -X 8 -y 12 -Y 12
-render_list -a -m maps -n 4 -z 6 -Z 6 -x 16 -X 16 -y 25 -Y 25
-render_list -a -m maps -n 4 -z 7 -Z 7 -x 32 -X 33 -y 50 -Y 51
-render_list -a -m maps -n 4 -z 8 -Z 8 -x 65 -X 67 -y 101 -Y 102
-render_list -a -m maps -n 4 -z 9 -Z 9 -x 130 -X 134 -y 202 -Y 204
-render_list -a -m maps -n 4 -z 10 -Z 10 -x 261 -X 269 -y 404 -Y 408
-render_list -a -m maps -n 4 -z 11 -Z 11 -x 522 -X 539 -y 809 -Y 817
-render_list -a -m maps -n 4 -z 12 -Z 12 -x 1044 -X 1078 -y 1618 -Y 1634
-render_list -a -m maps -n 4 -z 13 -Z 13 -x 2088 -X 2157 -y 3236 -Y 3269
-render_list -a -m maps -n 4 -z 14 -Z 14 -x 4177 -X 4314 -y 6472 -Y 6539
-render_list -a -m maps -n 4 -z 15 -Z 15 -x 8355 -X 8628 -y 12945 -Y 13078
-render_list -a -m maps -n 4 -z 16 -Z 16 -x 16711 -X 17257 -y 25891 -Y 26157
-render_list -a -m maps -n 4 -z 17 -Z 17 -x 33423 -X 34515 -y 51783 -Y 52315
-render_list -a -m maps -n 4 -z 18 -Z 18 -x 66846 -X 69031 -y 103567 -Y 104630
-render_list -a -m maps -n 4 -z 19 -Z 19 -x 133693 -X 138062 -y 207134 -Y 209260
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 4 -Z 4 -x 4 -X 4 -y 6 -Y 6
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 5 -Z 5 -x 8 -X 8 -y 12 -Y 12
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 6 -Z 6 -x 16 -X 16 -y 25 -Y 25
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 7 -Z 7 -x 32 -X 33 -y 50 -Y 51
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 8 -Z 8 -x 65 -X 67 -y 101 -Y 102
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 9 -Z 9 -x 130 -X 134 -y 202 -Y 204
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 10 -Z 10 -x 261 -X 269 -y 404 -Y 408
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 11 -Z 11 -x 522 -X 539 -y 809 -Y 817
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 12 -Z 12 -x 1044 -X 1078 -y 1618 -Y 1634
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 13 -Z 13 -x 2088 -X 2157 -y 3236 -Y 3269
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 14 -Z 14 -x 4177 -X 4314 -y 6472 -Y 6539
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 15 -Z 15 -x 8355 -X 8628 -y 12945 -Y 13078
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 16 -Z 16 -x 16711 -X 17257 -y 25891 -Y 26157
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 17 -Z 17 -x 33423 -X 34515 -y 51783 -Y 52315
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 18 -Z 18 -x 66846 -X 69031 -y 103567 -Y 104630
+render_list -a -m maps -n 4 -t /eosstracker/maps -z 19 -Z 19 -x 133693 -X 138062 -y 207134 -Y 209260
