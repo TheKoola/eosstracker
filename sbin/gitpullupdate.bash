892584bf92aa5394fccf6fb50b6adf30b4317ad9
TheKoola <33835798+TheKoola@users.noreply.github.com>
2024-06-30 20:23:10 -0600

    Added git-pull update bash script

---
+++ sbin/gitpullupdate.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# script to perform a git-pull on the eosstracker directory
+
+set -m
+
+# The local location for things
+HOMEDIR=/eosstracker
+
+# Check to make sure we are root
+ME=$(whoami)
+if [ ${ME} != "root" ]; then
+	echo "Not running as root...exiting"
+	exit
+fi
+
+# Inform user
+echo "Attempting to update the eosstracker software (timeout 20 seconds)..."
+
+# Perform the git pull
+su - eosstracker -c "cd ${HOMEDIR}; timeout --foreground 20 git pull -q"
+let ret=$?
+
+if [ $ret -eq 124 ]; then
+
+    # Git command timed out
+    echo "Eosstracker update command timed out.  Possibly no Internet connection..."
+    echo "Done."
+
+else
+
+    if [ $ret -eq 0 ]; then
+
+    # Git pull worked, but there are no new files
+    echo "Eosstracker software is up-to-date..."
+    echo "Done."
+
+    else
+
+    # Git downloaded files and performed updates
+    echo "Eosstracker software has been updated to the latest version..."
+    echo "Done."
+
+fi
